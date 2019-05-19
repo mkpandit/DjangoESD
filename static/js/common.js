d6facaafbb4eb9d367a3fc9f0f78b3024a789fac
mkpandit <manish.kumer.pandit@gmail.com>
2019-05-19 11:39:19 -0400

    CSS fix

--- static/js/common.js
+++ static/js/common.js
@@ -19,8 +19,8 @@ $(document).ready(function() {
 });
 
 $(function(){
-    $('div#header > .menu').on('click',function(event){
-        // event.preventDefault()
+    $('div#header > .menu > a.menu-click').on('click',function(event){
+        event.preventDefault()
         $(this).parent().find('ul').first().toggle(300);
         $(this).parent().siblings().find('ul').hide(200);
 
